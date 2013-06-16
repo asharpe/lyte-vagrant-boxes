12744e3b34f585410dedf17c8324b26db8623f13
Andrew Sharpe <andrew.sharpe.79@gmail.com>
2013-06-16 17:02:52 +1000

    Futzing to get some reliability on OSX
    On OSX I get frequent issues with DNS which stop the automation of the install (eg, fail to download kickstart, fail to download install.img).  Unfortunately using NAT appears to be the only really generic way to get to the VM (via port forwarding) to allow veewee to work, so this solution is likely very environment specific.
    
    The Veeweefile is purely to undo the extra interface I'm using to allow reliable networking between the guest and host.

--- definitions/sl6-64-lyte/definition.rb
+++ definitions/sl6-64-lyte/definition.rb
@@ -1,25 +1,32 @@
 Veewee::Session.declare({
-  :cpu_count => '2',
-  :memory_size=> '1024',
+  :cpu_count => '1',
+  :memory_size=> '768',
   :disk_size => '81920',
   :disk_format => 'VDI',
   :hostiocache => 'off',
   :virtualbox => { 
     :vm_options => [
-      "ioapic" => "on",
-      "pae" => "on"
+      :ioapic => 'on',
+      :pae => 'on',
+      :natdnsproxy1 => 'off',
+      :natdnshostresolver1 => 'on',
+      :nic2 => 'hostonly',
+      :nictype2 => '82540EM',
+		:cableconnected2 => 'on',
+      :hostonlyadapter2 => 'vboxnet0',
     ]
   },
   :os_type_id => 'RedHat_64',
-  :iso_file => "SL-64-x86_64-2013-03-18-boot.iso",
+#  :iso_file => "SL-64-x86_64-2013-03-18-boot.iso",
+  :iso_file => "SL-63-x86_64-2012-08-02-boot.iso",
   :iso_src => "http://mirror.aarnet.edu.au/pub/scientific/6.4/x86_64/iso/SL-64-x86_64-2013-03-18-boot.iso",
   :iso_md5 => "e9625e2c3b4b02d94ffa478773d5d58f",
   :iso_download_timeout => 1000,
   :boot_wait => "15",
-  :boot_cmd_sequence => [ '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
+  :boot_cmd_sequence => [ '<Tab> text ks=http://host.local.vm:%PORT%/ks.cfg.erb ksdevice=eth1<Enter>' ],
+  :kickstart_file => "ks.cfg.erb",
   :kickstart_port => "7122",
   :kickstart_timeout => 10000,
-  :kickstart_file => "ks.cfg",
   :ssh_login_timeout => "10000",
   :ssh_user => "vagrant",
   :ssh_password => "vagrant",
@@ -31,8 +38,11 @@ Veewee::Session.declare({
   :postinstall_files => [
     "base.sh",
     "vagrant.sh",
-    "virtualbox.sh",
+#    "virtualbox.sh",
     "cleanup.sh"
   ],
-  :postinstall_timeout => 10000
+  :postinstall_timeout => 10000,
+  :params => {
+    :proxy => 'http://host.local.vm:3142'
+  },
 })
